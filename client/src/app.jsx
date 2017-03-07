bb5f1fb65f428c60749affdfcac8f8e589aea2ac
Zaurbek Zhakupov <bigz3733@gmail.com>
2017-03-07 03:34:17 -0500

    1

---
+++ client/src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEvenPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { browserHistory, Router } from 'react-router';
+import routes from './routes.js';
+
+
+injectTapEventPlugin();
+
+ReactDOM.render(<MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={browserHistory} routes={routes}/>
+</MuiThemeProvider>, document.getElementById('react-app'));
---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Card, CardTitle} from 'material-ui/Card';
+
+
+const HomePage = () => (
+  <Card className='container'>
+    <Card Title='React Application' subtitle="this is the home page."/>
+  </Card>
+);
+
+
+export default HomePage;
